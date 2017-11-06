3072e63c72032372fbedb60e3fe6ba39d0c372f8
Chris Schmich <schmch@gmail.com>
2017-11-05 20:40:47 -0600

    Fix #10: Add more diagnostic output to deploy.sh.

--- deploy/deploy.sh
+++ deploy/deploy.sh
@@ -2,9 +2,12 @@
 
 set -euf -o pipefail
 
+echo Analyze repo and deploy locales.json.
+
 # Return to origin repo's deploy dir.
 cd $(dirname $0)/../..
 
+echo Add SSH key to authentication agent.
 chmod 400 deploy_key
 eval `ssh-agent -s`
 ssh-add deploy_key
@@ -15,16 +18,21 @@ project=$(basename "$url" .git)
 repo="git@github.com:$user/$project-localization"
 sha=`git rev-parse --verify HEAD`
 
+echo Clone localization repo $repo.
 git clone --depth 1 "$repo" out
+
+echo Generate locales.json.
 ruby chrome-extension-localization/deploy/gen-locales.rb > out/locales.json
 
 cd out && git add locales.json
 if [ -z "$(git diff --cached)" ]; then
-  echo 'No changes to deploy.'
+  echo No changes to deploy.
   exit 0
 fi
 
 git config user.name "$COMMIT_AUTHOR_NAME"
 git config user.email "$COMMIT_AUTHOR_EMAIL"
 git commit -a -m "Update locales.json for $user/$project@${sha}."
+
+echo Deploy changes.
 git push "$repo" master
